d75829f2fba17d9bdd197968319e35ddb51849b8
Filippo Alessandrini <filippoalessandrini.fa@gmail.com>
2024-08-07 19:57:08 +0200

    Updated contact link.

--- src/modules/order/components/help/index.tsx
+++ src/modules/order/components/help/index.tsx
@@ -1,6 +1,6 @@
-import { Heading, Text } from "@medusajs/ui"
+import { Heading } from "@medusajs/ui"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import React from "react"
-import { ArrowUpRightMini } from "@medusajs/icons"
 
 const Help = () => {
   return (
@@ -9,12 +9,7 @@ const Help = () => {
       <div className="text-base-regular my-2">
         <ul className="gap-y-2 flex flex-col">
           <li>
-          <a href="mailto:servizioclienti@sbcenter.it" className="flex gap-x-1 items-center group text-yellow-500 hover:text-yellow-600 transition-all duration-300">
-            <Text>
-              Contattaci
-            </Text>
-            <ArrowUpRightMini className="group-hover:rotate-45 transition-transform duration-300"/>
-          </a>
+            <a href="mailto:servizioclienti@sbcenter.it" className="text-ui-fg-subtle hover:text-ui-fg-base">Contattaci</a>
           </li>
         </ul>
       </div>
